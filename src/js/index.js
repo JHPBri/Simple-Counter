ee4a5cf367aad33c96193ca02f8cf58e19ef32aa
JHPBri <janghsuh@gmail.com>
2022-05-07 00:56:45 +0000

    test

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,45 @@
 //import react into the bundle
 import React from "react";
+import PropTypes from "prop-types";
 import ReactDOM from "react-dom";
 
 // include your styles into the webpack bundle
 import "../styles/index.css";
 
-//import your own components
-import Home from "./component/home.jsx";
+function SimpleCounter(props) {
+	return (
+		<div className="Countbar">
+			<h1 className="four">{props.digitfour}</h1>
+			<h1 className="three">{props.digitthree}</h1>
+			<h1 className="two">{props.digittwo}</h1>
+			<h1 className="one">{props.digitone}</h1>
+		</div>
+	);
+}
 
-//render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+SimpleCounter.PropTypes = {
+	digitfour: PropTypes.number,
+	digitthree: PropTypes.number,
+	digittwo: PropTypes.number,
+	digitone: PropTypes.number,
+};
+
+let counter = 0;
+
+export var Interval = setInterval(() => {
+	const four = Math.floor(counter / 1000);
+	const three = Math.floor(counter / 100);
+	const two = Math.floor(counter / 10);
+	const one = Math.floor(counter / 1);
+	console.log(four, three, two, one);
+	counter++;
+	ReactDOM.render(
+		<SimpleCounter
+			digitone={one}
+			digittwo={two}
+			digitthree={three}
+			digitfour={four}
+		/>,
+		document.querySelector("#app")
+	);
+}, 1000);
